928e0505cf9f98811d73cf8c6e17cfd14360d821
lucasqueiroz <sepulchrolucas@gmail.com>
2024-03-19 22:56:02 -0300

    refactor `nvim-cmp` to just complete on implicity confirmation

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,7 +3,10 @@ return {
   dependencies = { "hrsh7th/cmp-emoji" },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    local cmp = require("cmp")
     table.insert(opts.sources, { name = "emoji" })
+
+    -- Window customization
     opts.window = {
       completion = {
         border = "rounded",
@@ -27,5 +30,21 @@ return {
         auto_open = true,
       },
     }
+    opts.preselect = cmp.PreselectMode.None
+    opts.completion = {
+      completeopt = "menu,menuone,noinsert,noselect",
+    }
+    -- Remaps the key to only accept the suggestion when pressing <CR> implicitly
+    opts.mapping = cmp.mapping.preset.insert(vim.tbl_deep_extend("force", opts.mapping, {
+
+      -- Disable key arrows
+      ["<Up>"] = cmp.config.disable,
+      ["<Down>"] = cmp.config.disable,
+      -- Disable Enter
+      ["<CR>"] = cmp.config.disable,
+
+      ["<C-y>"] = cmp.mapping.confirm({ select = false }), -- 'select = false' to only confirm explicitly selected item
+      ["<S-CR>"] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }))
   end,
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,11 +2,11 @@ return {
   { "nyoom-engineering/oxocarbon.nvim" },
   { "rose-pine/neovim", name = "rose-pine", lazy = false },
   { "datsfilipe/vesper.nvim", lazy = false },
-  { "RRethy/base16-nvim", name = "base16", event = "VeryLazy" },
+  { "RRethy/base16-nvim", name = "base16" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "base16-black-metal-bathory",
+      colorscheme = "vesper",
     },
   },
 }
