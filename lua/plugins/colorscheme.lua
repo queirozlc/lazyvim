8528708b22bca099a474615a207221cab0cd8abe
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-03 08:53:56 -0300

    add aquarium and ayu dark theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,11 +2,13 @@ return {
   { "nyoom-engineering/oxocarbon.nvim" },
   { "rose-pine/neovim", name = "rose-pine", lazy = false },
   { "datsfilipe/vesper.nvim", lazy = false },
+  { "frenzyexists/aquarium-vim", name = "aquarium" },
+  { "ayu-theme/ayu-vim", name = "ayu" },
   { "RRethy/base16-nvim", name = "base16" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vesper",
+      colorscheme = "ayu",
     },
   },
 }
