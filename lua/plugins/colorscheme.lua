de5c0f6d63f59e718635acedd2ad09901a2a26d5
lucasqueiroz <sepulchrolucas@gmail.com>
2024-04-03 09:02:06 -0300

    add base46 themes
    Signed-off-by: lucasqueiroz <sepulchrolucas@gmail.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,13 +2,13 @@ return {
   { "nyoom-engineering/oxocarbon.nvim" },
   { "rose-pine/neovim", name = "rose-pine", lazy = false },
   { "datsfilipe/vesper.nvim", lazy = false },
-  { "frenzyexists/aquarium-vim", name = "aquarium" },
   { "ayu-theme/ayu-vim", name = "ayu" },
+  { "notken12/base46-colors", name = "base46" },
   { "RRethy/base16-nvim", name = "base16" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "ayu",
+      colorscheme = "rxyhn",
     },
   },
 }
