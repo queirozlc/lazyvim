b2d7e5afefacb82266f404323081e448d2fc7622
caslu <sepulchrolucas@gmail.com>
2024-06-18 18:16:48 -0300

    disable oil as a default file explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("oil").setup({
+      default_file_explorer = false,
       float = {
         max_height = 20,
         max_width = 60,
